1ba3de9810914d87cf4e849d417ee6991394553d
Flinn <flinn@actsasflinn.com>
2008-11-09 21:42:33 -0500

    updated look and feel

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,12 @@
-// Common JavaScript code across your application goes here.
+$().ready(function() {
+  url = 'http://search.twitter.com/search.json?q=hartfordrb&callback=?'
+  $.getJSON(url, function(data) {
+    results = data.results.slice(0,5);
+    $.each(results, function(n, result) {
+      user_url = 'http://twitter.com/'+result.from_user;
+      $('#tweets').
+        append($('<li class="tweet clearfix">').attr('id','tweet_'+result.id).html(
+          '<a href="'+user_url+'"><img src="'+ result.profile_image_url +'" alt="" /></a> '+result.text))
+    });
+  })
+})
